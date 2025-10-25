a0f6d67fd6476a36938ae28ef837d40c1be587bc
unknown <ramramturin@gmail.com>
2025-10-25 19:24:38 +0800

    Add my code for portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+// STYLES
+import './styles/main.css'
+import './styles/card.css'
+import './styles/responsive_layout.css'
+import './styles/single_page.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
